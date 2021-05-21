8921e2b58af1b712d6a19a3f40a095c6fe347449
Karmen Fraulein <karmnfraulein@gmail.com>
2021-05-21 18:37:41 +0400

    BoostrPage

---
+++ script.js
@@ -0,0 +1,13 @@
+// Sidebar toggle function
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
